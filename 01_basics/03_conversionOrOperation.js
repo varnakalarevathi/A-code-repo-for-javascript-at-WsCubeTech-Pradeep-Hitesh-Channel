75ecdb4adeba3ce83a4c1dd88a1ad9af31758298
varnakalarevathi <vknenterprises1968@gmail.com>
2025-09-13 11:46:00 +0000

    comparison

--- 01_basics/03_conversionOrOperation.js
+++ 01_basics/03_conversionOrOperation.js
@@ -11,20 +11,20 @@
 
 
  // let score='33'
- console.log(typeof score);  //string
- console.log(typeof (score));       //string
+//  console.log(typeof score);  //string
+//  console.log(typeof (score));       //string
  
 
  // let score='33'
  let valueInNumber=Number(score)  
-  console.log(typeof (score));        //string
- console.log(typeof valueInNumber);   //number   ----abserve the console below --carefully analyse the results of both
+//   console.log(typeof (score));        //string
+//  console.log(typeof valueInNumber);   //number   ----abserve the console below --carefully analyse the results of both
 
 
 //  let score='33abc'
- console.log(valueInNumber);      //NaN   ----abserve the console above --carefully analyse the results of both
+//  console.log(valueInNumber);      //NaN   ----abserve the console above --carefully analyse the results of both
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 
 // Notes on above conversions
@@ -42,7 +42,7 @@ let isLoggedIn ="Nagaraj"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn) 
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 =>  true     :  0 =>  false 
@@ -55,8 +55,76 @@ let someNumber=33
 
 let stringNumber= String(someNumber)
 
-console.log(someNumber);
-console.log(typeof stringNumber);
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+// *************************Operations****************************
+
+let value = 3
+let negValue=-value
+
+// console.log(negValue); ----//output -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="Hello"
+let str2=" Nagaraj"
+let str3 = str1 + str2      //Hello Nagaraj
+
+// console.log(str3);
+
+console.log(1+"2");   //12
+
+console.log("1"+2);   //12
+
+ console.log("1"+2+2);   //122
+
+console.log(1+2+ "2");   //32
+
+console.log(3+4*5%5);  //3        //first calculate multiplication
+
+
+console.log((3+4)*5%3);  //2          // as per math--from left
+
+console.log(true);      //true
+console.log(+true);     1
+
+console.log(-true);     //-1
+console.log(false);     //0
+console.log(-false);    //-0
+
+console.log("");        //blank since it is empty string
+
+console.log(+"");       //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 +2 
+
+let gameCounter=100
+gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ 01_basics/comparison.js
@@ -0,0 +1,27 @@
+// console.log(2>1);     //true
+// console.log(2<1);     //  false
+// console.log(2>=1);      //true
+// console.log(2<=1);      //false
+// console.log(2==1);      //false
+// console.log(2!=1);      //true
+
+
+// console.log("2" >1);     //true
+// console.log("02" >1);       //true
+
+// console.log(null);    //null
+// console.log(null>0);    //false
+// console.log(null==0);       //false
+// console.log(null>=0);       //true
+
+
+// console.log(undefined);    //undefined
+// console.log(undefined>0);    //false
+// console.log(undefined==0);       //false
+//  console.log(undefined>=0);       //false
+
+
+
+ console.log("2"===2);       //false
+ 
+
