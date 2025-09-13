9e156a1d63274e383b845ffc0f80a211714fa36e
varnakalarevathi <vknenterprises1968@gmail.com>
2025-09-13 07:06:34 +0000

    Add initial variable and data type examples

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 589425
+let accountEmail="nag@gmail.com"
+var accountPassword="123456"
+accountCity="Bengaluru"
+let accountState;
+
+// accountId=2           //this is not allowed
+
+
+   
+        
+
+
+accountEmail="ram123@gmail.com"
+accountPassword=85213
+accountCity="Mysuru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+//Prefer not to use var because of issue in block schope and functional scope.
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,41 @@
+"strict use"            //treate all JS codes as newer version
+
+// alert(3+3)           //we are using nodejs, not browser
+
+console.log(3
+    +3);                //code readability should be high
+
+    console.log(3+3);
+
+   let name="Nagaraj"
+   let age=65
+   let isLoggedIn="false"
+
+//    Data Types
+
+// a. primitive type
+
+
+// number ==> 2 to the  power of 53
+//bigint
+//string ==> ""
+//boolean ==> true/false
+// null ==> satandalone value   (it like empty)
+// undefined ==>     --      example let state;
+//symbol  ==> unique
+
+
+
+// b. non-primitive type data
+
+//object
+    
+    console.log(typeof "Nagaraj");    //string
+    console.log(typeof age);        //number
+    console.log(typeof null);           //object
+    console.log(typeof undefined);      //undefined
+    
+    
+    
+    
+    
